e56d6e637adbd4acdc93919666323df590d92591
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2022-10-29 21:13:52 +0300

    init

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,9 @@
 import { inject, injectable } from 'inversify';
-import express, { Express } from 'express';
+import express, { Express, json } from 'express';
 import { Server } from 'http';
 import { injectKeys } from './constants';
-import { ILoggerService, IChalkService, IConfigService } from './services';
+import { ILoggerService, IChalkService, IConfigService, IPrismaService } from './services';
+import cookieParser from 'cookie-parser';
 
 @injectable()
 export class App {
@@ -13,16 +14,33 @@ export class App {
     @inject(injectKeys.ILoggerService) private logger: ILoggerService,
     @inject(injectKeys.IChalkService) private chalk: IChalkService,
     @inject(injectKeys.IConfigService) private config: IConfigService,
+    @inject(injectKeys.IPrismaService) private prisma: IPrismaService,
   ) {
     this.app = express();
     this.port = this.config.get('PORT');
   }
 
-  init(): void {
+  useMiddleWare(): void {
+    this.app.use(json());
+    this.app.use(cookieParser());
+  }
+
+  startServer(): void {
     this.server = this.app.listen(this.port, () => {
       this.logger.info(
         `${this.chalk.highlight('The server has been running on')} http://localhost:${this.port}`,
       );
     });
+  }
+
+  closeServer(): void {
+    this.server.close();
+  }
+
+  async init(): Promise<void> {
+    await this.prisma.connect();
+    this.useMiddleWare();
+
+    this.startServer();
   }
 }
